7b07a341113f4ea7de11551e0bcca9ae73c460ce
Ray Torres <theonliray@hotmail.com>
2026-02-11 12:56:03 -0600

    chore(hsm): change is_temporary_id default from false to true

--- migrations/012_hsm_slots.up.sql
+++ migrations/012_hsm_slots.up.sql
@@ -12,7 +12,7 @@ CREATE TABLE hsm_slots (
     id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
     label VARCHAR(255) NOT NULL,
     slot_number INT NOT NULL,
-    is_temporary_id BOOLEAN NOT NULL DEFAULT FALSE,
+    is_temporary_id BOOLEAN NOT NULL DEFAULT TRUE,
     -- Split AES-GCM encrypted PIN into its components: IV (nonce), ciphertext and auth tag
     pin_iv VARBINARY(12) NULL COMMENT 'IV/nonce for AES-GCM (12 bytes)',
     pin_ciphertext VARBINARY(1024) NULL COMMENT 'AES-GCM ciphertext of the PIN',
